66bb7ea3ea7fc772e8e79a47dd089cdaf91f4b25
juliagalaton <iulia.galaton@gmail.com>
2025-01-13 15:07:04 +0100

    display products from the recipe

--- script.js
+++ script.js
@@ -2,7 +2,50 @@ const productsContainer = document.getElementById("products-container");
 
 
 
-const products = ["milk", "sugar", "flour", "eggs", "chocolate", "banana", "buttermilk"];
+let products = [];
+
+class Recipe {
+    name;
+    type;
+    time;
+    recipeIngredients = [];
+
+    constructor(name, type, time, recipeIngredients){
+        this.name = name;
+        this.type = type;
+        this.time = time;
+        this.recipeIngredients = recipeIngredients;
+    }
+
+    getIngredients = () => {
+      return this.recipeIngredients.map(element => element.ingredient)
+    
+    }
+}
+
+const pancakes = new Recipe("Buttermilk pancakes", "desserts", "30 mins", [
+    {
+        ingredient: "buttermilk",
+        quantity: "1.3 cups"
+    },
+    {
+        ingredient: "flour",
+        quantity: "2 cups"
+    },
+    {
+        ingredient: "eggs",
+        quantity: "2"
+    },
+    {
+        ingredient: "sugar",
+        quantity: "0.3 cups"
+    },
+    {
+        ingredient: "salt",
+        quantity: "1 tsp "
+    },
+
+]);
 
 const displayProducts = () => {
     const productBtn =  products.map(product => {
@@ -12,9 +55,14 @@ const displayProducts = () => {
     productsContainer.innerHTML = productBtn;
 }
 
+products = pancakes.getIngredients();
+
+console.log("Products from the recipe",products);
+
 displayProducts();
 
 let productBtns = document.querySelectorAll("button");
+
 productBtns.forEach((btn) => {
     btn.addEventListener('click', () => {
       console.log(btn);
